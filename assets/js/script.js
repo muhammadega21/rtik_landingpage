9376ba83e25700e4bf7ee2d716f7fad9664a9492
muhammadega21 <dermawane988@gmail.com>
2025-09-01 15:07:18 +0700

    asd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,74 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".card.hidden");
-  const loadMoreBtn = document.getElementById("load-more");
-  const step = 3; // jumlah yang ditampilkan setiap klik
-  let index = 0;
+const navbar = document.querySelector("nav");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link a");
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.querySelector(".nav-mobile-menu");
 
-  loadMoreBtn.addEventListener("click", function () {
-    for (let i = 0; i < step; i++) {
-      if (index < cards.length) {
-        cards[index].classList.remove("hidden");
-        cards[index].classList.add("show");
-        index++;
-      }
+// Navbar
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (mobileMenu.classList.contains("show")) {
+      mobileMenu.classList.remove("show");
+      setTimeout(() => {
+        mobileMenu.style.display = "none";
+      }, 300);
     }
 
-    if (index >= cards.length) {
-      loadMoreBtn.style.display = "none";
+    window.scrollTo({
+      top: targetElement.offsetTop - 70,
+      behavior: "smooth",
+    });
+  });
+});
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    let sectionTop = section.offsetTop - 150;
+    let sectionHeight = section.offsetHeight;
+
+    if (scrollTop >= sectionTop && scrollTop < sectionTop + sectionHeight) {
+      let id = section.getAttribute("id");
+
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${id}`) {
+          link.classList.add("active");
+        }
+      });
     }
   });
 });
 
-//bagian faq
-document.addEventListener("DOMContentLoaded", function () {
-  const faqItems = document.querySelectorAll(".faq-item");
+// Mobile menu
+menuToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
 
-  faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
-    question.addEventListener("click", () => {
-      // Tutup semua sebelum buka yang diklik
-      faqItems.forEach((i) => {
-        if (i !== item) i.classList.remove("active");
-      });
+  if (mobileMenu.classList.contains("show")) {
+    mobileMenu.classList.remove("show");
+    setTimeout(() => {
+      mobileMenu.style.display = "none";
+    }, 300);
+  } else {
+    mobileMenu.style.display = "flex";
+    void mobileMenu.offsetWidth;
+    mobileMenu.classList.add("show");
+  }
+});
 
-      item.classList.toggle("active");
-    });
-  });
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.closest(".nav-mobile") &&
+    mobileMenu.classList.contains("show")
+  ) {
+    mobileMenu.classList.remove("show");
+    setTimeout(() => {
+      mobileMenu.style.display = "none";
+    }, 300);
+  }
 });
